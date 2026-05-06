0188ff2846f6c8dcfa41575502895abd85ca600a
Zainul Saifi <saifizainul9990@gmail.com>
2026-05-06 12:02:46 +0530

    Refactor weather fetching logic and error handling

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const apiKey = "f5c0f4159870cdd7bff82ae073fedf0b";
 
 async function getWeather() {
+
     const city = document.getElementById("cityInput").value;
 
-    if(city === "") {
+    if (city === "") {
         alert("Please enter a city name");
         return;
     }
@@ -11,21 +12,32 @@ async function getWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
+
         const response = await fetch(url);
         const data = await response.json();
 
-        if(data.cod === "404") {
+        if (data.cod == "404") {
             alert("City not found");
             return;
         }
 
-        document.getElementById("cityName").innerText = `${data.name}, ${data.sys.country}`;
-        document.getElementById("temperature").innerText = `🌡 Temperature: ${data.main.temp} °C`;
-        document.getElementById("description").innerText = `☁ Weather: ${data.weather[0].description}`;
-        document.getElementById("humidity").innerText = `💧 Humidity: ${data.main.humidity}%`;
-        document.getElementById("wind").innerText = `🌬 Wind Speed: ${data.wind.speed} m/s`;
+        document.getElementById("cityName").innerText =
+            `${data.name}, ${data.sys.country}`;
 
-    } catch(error) {
+        document.getElementById("temperature").innerText =
+            `🌡 Temperature: ${data.main.temp} °C`;
+
+        document.getElementById("description").innerText =
+            `☁ Weather: ${data.weather[0].description}`;
+
+        document.getElementById("humidity").innerText =
+            `💧 Humidity: ${data.main.humidity}%`;
+
+        document.getElementById("wind").innerText =
+            `🌬 Wind Speed: ${data.wind.speed} m/s`;
+
+    } catch (error) {
         alert("Something went wrong");
+        console.log(error);
     }
 }
